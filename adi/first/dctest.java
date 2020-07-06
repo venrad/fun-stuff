b2ec1f8d785258066d08283a978dd10da0c4d502
Venkatesh Sanklapur <v_sanklapur@apple.com>
2020-07-06 13:52:23 -0700

    added dctest.java

---
+++ adi/first/dctest.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class dctest {
+  public static void main(String[] args) {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      reader.read();
+  }
+}
